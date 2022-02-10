57f6adc724068615788643e23e753d43c9a292af
Jesus Eduardo Perez <jesuseduardoperez@jesuss-macbook-pro.local>
2022-02-10 13:45:41 -0600

    commit message

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  "collectCoverge": true,
+  "collectCoverageFrom": [
+    "./src/**/*.js",
+    "!./src/**/serviceToService.js",
+    "!./src/static/**",
+  ],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    'dist',
+    'node_moduels',
+  ]
 };
